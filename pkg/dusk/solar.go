7bc13054ee7a9cdc97228474ca742b36ac49c3ae
Michael Roberts <michael@observerly.com>
2022-02-13 14:15:45 +0000

    chore: Updated DocStrings for various solar funcs.

--- pkg/dusk/solar.go
+++ pkg/dusk/solar.go
@@ -5,7 +5,7 @@ import "math"
 /*
 	GetSolarMeanAnomaly()
 
-	@param J - the Ephemeris Time or the number of centuries since J2000 epoch
+	@param J - the Ephemeris time or the number of centuries since J2000 epoch
 	@returns the non-uniform or anomalous apparent motion of the Sun along the plane of the ecliptic
 	@see EQ.47.3 p.338 of Meeus, Jean. 1991. Astronomical algorithms. Richmond, Va: Willmann-Bell
 */
@@ -17,10 +17,11 @@ func GetSolarMeanAnomaly(J float64) float64 {
 /*
 	GetSolarEquationOfCenter()
 
-	@param M - the Mean Solar Anomaly for the Ephemeris Time or the number of centuries since J2000 epoch
+	@param M - the mean solar anomaly for the Ephemeris time or the number of centuries since J2000 epoch
 	@returns the equation of center for the Sun
 	@see p.164 of Meeus, Jean. 1991. Astronomical algorithms. Richmond, Va: Willmann-Bell.
 */
 func GetSolarEquationOfCenter(M float64) float64 {
+	// applies modulo correction to the angle, and ensures always positive:
 	return 1.9148*sinx(M) + 0.0200*sinx(2*M) + 0.0003*sinx(3*M)
 }
