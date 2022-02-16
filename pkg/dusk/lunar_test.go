8b627884ff443dd1de452411d758384ef535113a
Michael Roberts <michael@observerly.com>
2022-02-16 14:51:54 +0000

    feat: Added GetLunarMeanLongitude() to dusk module.
    feat: Added GetLunarMeanLongitude() to dusk module. Includes associated test suite for module export definition and expected output.

--- pkg/dusk/lunar_test.go
+++ pkg/dusk/lunar_test.go
@@ -5,6 +5,17 @@ import (
 	"testing"
 )
 
+func TestGetLunarMeanLongitude(t *testing.T) {
+	var J float64 = GetCurrentJulianCenturyRelativeToJ2000(d)
+
+	var got float64 = GetLunarMeanLongitude(J)
+
+	var want float64 = 134.290182
+
+	if math.Abs(got-want) > 0.00001 {
+		t.Errorf("got %f, wanted %f", got, want)
+	}
+}
 func TestGetLunarMeanAnomaly(t *testing.T) {
 	var J float64 = GetCurrentJulianCenturyRelativeToJ2000(d)
 
