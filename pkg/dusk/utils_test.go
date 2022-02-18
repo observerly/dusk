54c8ba2ea39672372e5b02f61607b9deebdf3f1f
Michael Roberts <michael@observerly.com>
2022-02-18 16:21:33 +0000

    feat: Added GetMeanObliquityOfTheEcliptic() to dusk module.
    feat: Added GetMeanObliquityOfTheEcliptic() to dusk module. Includes associated test suite for module export definition and expected output.

--- pkg/dusk/utils_test.go
+++ pkg/dusk/utils_test.go
@@ -3,12 +3,31 @@ package dusk
 import (
 	"math"
 	"testing"
+	"time"
 )
 
 func TestGetEarthObliquity(t *testing.T) {
 	var got float64 = GetEarthObliquity()
 
 	var want float64 = 23.4397
+
+	if math.Abs(got-want) > 0.00001 {
+		t.Errorf("got %f, wanted %f", got, want)
+	}
+}
+
+func TestGetMeanObliquityOfTheEcliptic(t *testing.T) {
+	// For testing we need to specify a date because most calculations are
+	// differential w.r.t a time component. We set it to the date provided
+	// on p.148 of Meeus, Jean. 1991. Astronomical algorithms.Richmond,
+	// Va: Willmann - Bell.:
+	var d time.Time = time.Date(1987, 4, 10, 0, 0, 0, 0, time.UTC)
+
+	var J float64 = GetCurrentJulianCenturyRelativeToJ2000(d)
+
+	var got float64 = GetMeanObliquityOfTheEcliptic(J)
+
+	var want float64 = 23.440947
 
 	if math.Abs(got-want) > 0.00001 {
 		t.Errorf("got %f, wanted %f", got, want)
