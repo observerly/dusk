0a729aa5280a6b4aed491e90c6b2a6c13f6eef31
Michael Roberts <michael@observerly.com>
2022-02-22 11:41:43 +0000

    feat: Added GetHourAngle() to dusk module.
    feat: Added GetHourAngle() to dusk module. Includes associated test suite for module export definition and expected output.

---
+++ pkg/dusk/astrometry_test.go
@@ -0,0 +1,30 @@
+package dusk
+
+import (
+	"math"
+	"testing"
+)
+
+func TestGetHourAngle(t *testing.T) {
+	var LST float64 = GetLocalSiderealTime(datetime, longitude)
+
+	var got float64 = GetHourAngle(88.7929583, LST)
+
+	var want float64 = 347.6988036852858
+
+	if math.Abs(got-want) > 0.00001 {
+		t.Errorf("got %f, wanted %f", got, want)
+	}
+}
+
+func TestGetHourAngleBonus(t *testing.T) {
+	var LST float64 = GetLocalSiderealTime(d, longitude)
+
+	var got float64 = GetHourAngle(88.7929583, LST)
+
+	var want float64 = 316.181173
+
+	if math.Abs(got-want) > 0.00001 {
+		t.Errorf("got %f, wanted %f", got, want)
+	}
+}
