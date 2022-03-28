2933f98742c46c24c636bb6904622b7aef93356e
Michael J. Roberts <michael@observerly.com>
2022-03-28 18:23:24 +0100

    test: Added two new tests for ConvertEclipticCoordinateToEquatorial().
    test: Added two new tests for ConvertEclipticCoordinateToEquatorial().

--- pkg/dusk/coordinates_test.go
+++ pkg/dusk/coordinates_test.go
@@ -3,6 +3,7 @@ package dusk
 import (
 	"math"
 	"testing"
+	"time"
 )
 
 func TestConvertEclipticCoordinateToEquatorialRA(t *testing.T) {
@@ -18,6 +19,22 @@ func TestConvertEclipticCoordinateToEquatorialRA(t *testing.T) {
 	}
 }
 
+func TestConvertEclipticCoordinateToEquatorialRAAlt(t *testing.T) {
+	// Date of observation:
+	var datetime time.Time = time.Date(2015, time.Month(1), 1, 0, 0, 0, 0, time.UTC)
+
+	// utilising the ecliptic position of the moon on the datetime provided:
+	var ec EquatorialCoordinate = ConvertEclipticCoordinateToEquatorial(datetime, EclipticCoordinate{λ: 50.279952, β: -2.981288, Δ: 0})
+
+	var got float64 = ec.α
+
+	var want float64 = 48.662544
+
+	if math.Abs(got-want) > 0.15 {
+		t.Errorf("got %f, wanted %f", got, want)
+	}
+}
+
 func TestConvertEclipticCoordinateToEquatorialDec(t *testing.T) {
 	// utilising the ecliptic position of the moon on the datetime provided:
 	var ec EquatorialCoordinate = ConvertEclipticCoordinateToEquatorial(d, EclipticCoordinate{λ: 133.162655, β: -3.229126, Δ: 0})
@@ -25,6 +42,22 @@ func TestConvertEclipticCoordinateToEquatorialDec(t *testing.T) {
 	var got float64 = ec.δ
 
 	var want float64 = 13.768368
+
+	if math.Abs(got-want) > 0.15 {
+		t.Errorf("got %f, wanted %f", got, want)
+	}
+}
+
+func TestConvertEclipticCoordinateToEquatorialDecAlt(t *testing.T) {
+	// Date of observation:
+	var datetime time.Time = time.Date(2015, time.Month(1), 1, 0, 0, 0, 0, time.UTC)
+
+	// utilising the ecliptic position of the moon on the datetime provided:
+	var ec EquatorialCoordinate = ConvertEclipticCoordinateToEquatorial(datetime, EclipticCoordinate{λ: 50.279952, β: -2.981288, Δ: 0})
+
+	var got float64 = ec.δ
+
+	var want float64 = 14.941252
 
 	if math.Abs(got-want) > 0.15 {
 		t.Errorf("got %f, wanted %f", got, want)
